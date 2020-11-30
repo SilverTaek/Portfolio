8997f237533dde43235bc63ef280c4e08986e4a0
rudus1012 <rudus1012@naver.com>
2020-11-30 17:42:04 +0900

    JS:Navbar menu click

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(`navbarHeight: ${navbarHeight}`);
-  console.log(window.scrollY);
-
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
